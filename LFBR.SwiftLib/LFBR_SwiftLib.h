e47762062a7de72588f99ba98b2f67b9d6ca12a1
Luis Fernando Bustos Ramírez <lufe.bura@gmail.com>
2019-03-15 18:56:21 -0600

    Initial project setup

---
+++ LFBR.SwiftLib/LFBR_SwiftLib.h
@@ -0,0 +1,19 @@
+//
+//  LFBR_SwiftLib.h
+//  LFBR.SwiftLib
+//
+//  Created by Luis Fernando Bustos Ramírez on 3/15/19.
+//  Copyright © 2019 Gastando Tenis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LFBR_SwiftLib.
+FOUNDATION_EXPORT double LFBR_SwiftLibVersionNumber;
+
+//! Project version string for LFBR_SwiftLib.
+FOUNDATION_EXPORT const unsigned char LFBR_SwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LFBR_SwiftLib/PublicHeader.h>
+
+
---
+++ LFBR.SwiftLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LFBR.SwiftLibExamples
+//
+//  Created by Luis Fernando Bustos Ramírez on 3/15/19.
+//  Copyright © 2019 Gastando Tenis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
